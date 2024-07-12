dd1bf7460262a4f15ebac3e8ce530f17eba05c8e
mazhuventhan <mazhuventhan.s@dci.in>
2024-07-12 17:50:03 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-// import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Header from './Components/Header';
-// import Footer from './Components/Footer';
-// import FootBox from './Components/boxFoot';
-// import Home from './pages/Home/home';
-// import Login from './pages/Login/login';
-// // import ProtectedRoute from './Components/protectedRoute';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/header' element={<Header />} />
-//         <Route path='/footer' element={<Footer />} />
-//         <Route path='/footbox' element={<FootBox />} />
-//         <Route path='/' element={<Home />} />
-//         <Route path='/login' element={<Login />} />
-
-
-//         {/* <Route path="/home" element={<ProtectedRoute ><Home /></ProtectedRoute>} /> */}
-
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
@@ -36,6 +8,8 @@ import Login from './pages/Login/login';
 import HelmetWrapper from './Components/helmetWrapper';
 // import ProtectedRoute from './Components/protectedRoute';
 
+import {seoTitle,seoDescription} from "../src/Config/seo";
+
 function App() {
   return (
     <Router>
@@ -44,7 +18,7 @@ function App() {
           <Route path='/header' element={<Header />} />
           <Route path='/footer' element={<Footer />} />
           <Route path='/footbox' element={<FootBox />} />
-          <Route path='/' element={<Home title="Home - My Awesome App" description="Welcome to the home page of my awesome app" />} />
+          <Route path='/' element={<Home title={seoTitle.home} description={seoDescription.home} />} />
           <Route path='/login' element={<Login title="Login - My Awesome App" description="Login to access your account on my awesome app" />} />
           {/* <Route path="/home" element={<ProtectedRoute ><Home /></ProtectedRoute>} /> */}
         </Route>
---
+++ src/Config/seo.js
@@ -0,0 +1,9 @@
+export const seoTitle={
+    //home page title
+    home:"Home title"
+}
+
+export const seoDescription={
+    //home page description
+    home:"Welcome to the home page"
+}
